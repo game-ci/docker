396a2ef8019777d505ef56a920acbff94af85a50
Webber <webber@takken.io>
2020-10-02 20:18:30 +0200

    Remove old bootstrapper

--- bootstrapper.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# start clicker in background
-echo "starting clicker"
-./no-really-lets-click-everywhere.sh &
-
-# start unity hub
-echo "starting unity hub"
-xvfb-run -e /dev/stdout /opt/unity/UnityHub
