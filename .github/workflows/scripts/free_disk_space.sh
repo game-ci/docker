e539f4c7f142877432b60ce2037635baefe88425
Webber <webber@takken.io>
2020-10-12 22:13:26 +0200

    Quicker disk space wins

--- .github/workflows/scripts/free_disk_space.sh
+++ .github/workflows/scripts/free_disk_space.sh
@@ -1,50 +1,35 @@
 #!/usr/bin/env bash
 #
-# Source: https://github.com/apache/flink/blob/master/tools/azure-pipelines/free_disk_space.sh
-#
-# Licensed to the Apache Software Foundation (ASF) under one or more
-# contributor license agreements.  See the NOTICE file distributed with
-# this work for additional information regarding copyright ownership.
-# The ASF licenses this file to You under the Apache License, Version 2.0
-# (the "License"); you may not use this file except in compliance with
-# the License.  You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-
-#
-# The Azure provided machines typically have the following disk allocation:
-# Total space: 85GB
-# Allocated: 67 GB
-# Free: 17 GB
-# This script frees up 28 GB of disk space by deleting unneeded packages and
-# large directories.
-# The Flink end to end tests download and generate more than 17 GB of files,
-# causing unpredictable behavior and build failures.
+# Based on: https://github.com/apache/flink/blob/master/tools/azure-pipelines/free_disk_space.sh
 #
 echo "=============================================================================="
 echo "Freeing up disk space on CI system"
 echo "=============================================================================="
 
-echo "Listing 100 largest packages"
-dpkg-query -Wf '${Installed-Size}\t${Package}\n' | sort -n | tail -n 100
+# Before
+echo "Disk space before:"
 df -h
-echo "Removing large packages"
-sudo apt-get remove -y '^ghc-8.*'
-sudo apt-get remove -y '^dotnet-.*'
-sudo apt-get remove -y '^llvm-.*'
-sudo apt-get remove -y 'php.*'
-sudo apt-get remove -y azure-cli google-cloud-sdk hhvm google-chrome-stable firefox powershell mono-devel
-sudo apt-get autoremove -y
-sudo apt-get clean
-df -h
+
+#echo "Listing 100 largest packages"
+#dpkg-query -Wf '${Installed-Size}\t${Package}\n' | sort -n | tail -n 100
+#echo "Removing large packages"
+#sudo apt-get remove -y '^ghc-8.*'
+#sudo apt-get remove -y '^dotnet-.*'
+#sudo apt-get remove -y '^llvm-.*'
+#sudo apt-get remove -y 'php.*'
+#sudo apt-get remove -y azure-cli google-cloud-sdk hhvm google-chrome-stable firefox powershell mono-devel
+#sudo apt-get autoremove -y
+#sudo apt-get clean
+#df -h
+
 echo "Removing large directories"
 # deleting 15GB
 rm -rf /usr/share/dotnet/
+
+# https://github.com/actions/virtual-environments/issues/709#issuecomment-612569242
+rm -rf "/usr/local/share/boost"
+rm -rf "$AGENT_TOOLSDIRECTORY"
+
+# After
+echo "Disk space after:"
 df -h
