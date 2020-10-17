fb35880264e92a188116a5ba08934f9b7d12691b
Webber <webber@takken.io>
2020-10-17 03:27:47 +0200

    Make action async

--- .github/workflows/actions/report-to-backend/index.js
+++ .github/workflows/actions/report-to-backend/index.js
@@ -5,92 +5,96 @@ const startedEndpoint = 'https://europe-west3-unity-ci-versions.cloudfunctions.n
 const failedEndpoint = 'https://europe-west3-unity-ci-versions.cloudfunctions.net/reportBuildFailure';
 const publishedEndpoint = 'https://europe-west3-unity-ci-versions.cloudfunctions.net/reportPublication';
 
-const token = core.getInput('token', { required: true });
-const jobId = core.getInput('jobId', { required: true });
-const buildId = core.getInput('buildId', { required: true });
-const status = core.getInput('status', { required: true })
+const action = async () => {
+  const token = core.getInput('token', { required: true });
+  const jobId = core.getInput('jobId', { required: true });
+  const buildId = core.getInput('buildId', { required: true });
+  const status = core.getInput('status', { required: true })
 
-const headers = {
-  'Authorization': `Bearer ${token}`,
-  'Content-Type': 'application/json',
-};
+  const headers = {
+    'Authorization': `Bearer ${token}`,
+    'Content-Type': 'application/json',
+  };
 
-if (status === 'started') {
-  try {
-    const imageType = core.getInput('imageType', { required: true });
-    const baseOs = core.getInput('baseOs', { required: true });
-    const repoVersion = core.getInput('repoVersion', { required: true });
-    const unityVersion = core.getInput('unityVersion', { required: false }) || 'none';
-    const targetPlatform = core.getInput('targetPlatform', { required: false }) || 'none';
+  if (status === 'started') {
+    try {
+      const imageType = core.getInput('imageType', { required: true });
+      const baseOs = core.getInput('baseOs', { required: true });
+      const repoVersion = core.getInput('repoVersion', { required: true });
+      const unityVersion = core.getInput('unityVersion', { required: false }) || 'none';
+      const targetPlatform = core.getInput('targetPlatform', { required: false }) || 'none';
 
-    body = {
-      jobId,
-      buildId,
-      imageType,
-      baseOs,
-      repoVersion,
-      unityVersion,
-      targetPlatform,
-    }
-
-    const { statusCode, data } = await post(startedEndpoint, {headers, body});
-    console.log('Reported that this build has started.', statusCode, data);
-  } catch (err) {
-    console.error('An error occurred while reporting the start of this build.', err.statusCode, err.message);
-    console.error('~> data:', err.data);
-  }
-
-  return;
-}
-
-if (status === 'failed') {
-  try {
-    const reason = core.getInput('reason', { required: true });
-
-    body = {
-      buildId,
-      failure: {
-        reason,
+      const body = {
+        jobId,
+        buildId,
+        imageType,
+        baseOs,
+        repoVersion,
+        unityVersion,
+        targetPlatform,
       }
+
+      const { statusCode, data } = await post(startedEndpoint, {headers, body});
+      console.log('Reported that this build has started.', statusCode, data);
+    } catch (err) {
+      console.error('An error occurred while reporting the start of this build.', err.statusCode, err.message);
+      console.error('~> data:', err.data);
     }
 
-    const { statusCode, data } = await post(failedEndpoint, {headers, body});
-    console.log('Successfully reported the build failure.', statusCode, data);
-  } catch (err) {
-    console.error('An error occurred while reporting the build failure.', err.statusCode, err.message);
-    console.error('~> data:', err.data);
+    return;
   }
 
-  return;
-}
+  if (status === 'failed') {
+    try {
+      const reason = core.getInput('reason', { required: true });
 
-if (status === 'published') {
-  try {
-    const imageRepo = core.getInput('imageRepo', { required: true });
-    const imageName = core.getInput('imageName', { required: true });
-    const friendlyTag = core.getInput('friendlyTag', { required: true });
-    const specificTag = core.getInput('specificTag', { required: true });
-    const digest = core.getInput('digest', { required: true });
-
-    body = {
-      buildId,
-      dockerPublicationInfo: {
-        imageRepo,
-        imageName,
-        friendlyTag,
-        specificTag,
-        digest,
+      const body = {
+        buildId,
+        failure: {
+          reason,
+        }
       }
+
+      const { statusCode, data } = await post(failedEndpoint, {headers, body});
+      console.log('Successfully reported the build failure.', statusCode, data);
+    } catch (err) {
+      console.error('An error occurred while reporting the build failure.', err.statusCode, err.message);
+      console.error('~> data:', err.data);
     }
 
-    const { statusCode, data } = await post(publishedEndpoint, {headers, body});
-    console.log('Successfully reported this publication.', statusCode, data);
-  } catch (err) {
-    console.error('An error occurred while reporting this publication.', err.statusCode, err.message);
-    console.error('~> data:', err.data);
+    return;
   }
 
-  return;
+  if (status === 'published') {
+    try {
+      const imageRepo = core.getInput('imageRepo', { required: true });
+      const imageName = core.getInput('imageName', { required: true });
+      const friendlyTag = core.getInput('friendlyTag', { required: true });
+      const specificTag = core.getInput('specificTag', { required: true });
+      const digest = core.getInput('digest', { required: true });
+
+      const body = {
+        buildId,
+        dockerPublicationInfo: {
+          imageRepo,
+          imageName,
+          friendlyTag,
+          specificTag,
+          digest,
+        }
+      }
+
+      const { statusCode, data } = await post(publishedEndpoint, {headers, body});
+      console.log('Successfully reported this publication.', statusCode, data);
+    } catch (err) {
+      console.error('An error occurred while reporting this publication.', err.statusCode, err.message);
+      console.error('~> data:', err.data);
+    }
+
+    return;
+  }
+
+  throw new Error('Invalid status was given, so no action was taken.');
 }
 
-throw new Error('Invalid status was given, so no action was taken.');
+action().catch((err) => { core.setFailed(err) });
