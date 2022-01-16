8b1af65116bde9d5e5e769db8eb72f616df76ec0
Webber <webber@takken.io>
2022-01-16 02:53:50 +0100

    fix: add more disk space

--- .github/workflows/scripts/free_disk_space.sh
+++ .github/workflows/scripts/free_disk_space.sh
@@ -13,14 +13,14 @@ df -h
 #echo "Listing 100 largest packages"
 #dpkg-query -Wf '${Installed-Size}\t${Package}\n' | sort -n | tail -n 100
 #echo "Removing large packages"
-#sudo apt-get remove -y '^ghc-8.*'
+sudo apt-get remove -y '^ghc-8.*'
 #sudo apt-get remove -y '^dotnet-.*'
 #sudo apt-get remove -y '^llvm-.*'
-#sudo apt-get remove -y 'php.*'
+sudo apt-get remove -y 'php.*'
 #sudo apt-get remove -y azure-cli google-cloud-sdk hhvm google-chrome-stable firefox powershell mono-devel
-#sudo apt-get autoremove -y
-#sudo apt-get clean
-#df -h
+sudo apt-get autoremove -y
+sudo apt-get clean
+df -h
 
 echo "Removing large directories"
 # https://github.com/apache/flink/blob/master/tools/azure-pipelines/free_disk_space.sh
