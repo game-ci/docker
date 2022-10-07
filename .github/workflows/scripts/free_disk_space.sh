64778e24a6adbdeaf9c4412b361f6487dec750f5
Webber Takken <webber@takken.io>
2022-10-07 18:40:36 +0200

    chore: free up more disk space (#186)
    * chore: free up more disk space
    
    * chore: temp change trigger
    
    * chore: remove more folders using script
    
    * fix: push
    
    * chore: speed up
    
    * chore: better formatting
    
    * chore: [skip ci] restore original trigger

--- .github/workflows/scripts/free_disk_space.sh
+++ .github/workflows/scripts/free_disk_space.sh
@@ -5,30 +5,46 @@
 echo "=============================================================================="
 echo "Freeing up disk space on CI system"
 echo "=============================================================================="
+echo ""
 
 # Before
 echo "Disk space before:"
 df -h
 
-#echo "Listing 100 largest packages"
-#dpkg-query -Wf '${Installed-Size}\t${Package}\n' | sort -n | tail -n 100
+# List packages
+#echo "Listing 25 largest packages"
+#dpkg-query -Wf '${Installed-Size}\t${Package}\n' | sort -n | tail -n 25
+#echo ""
+
+# Remove packages
+#echo ""
 #echo "Removing large packages"
-sudo apt-get remove -y '^ghc-8.*'
+#sudo apt-get remove -y '^ghc-8.*'
 #sudo apt-get remove -y '^dotnet-.*'
 #sudo apt-get remove -y '^llvm-.*'
-sudo apt-get remove -y 'php.*'
+#sudo apt-get remove -y 'php.*'
 #sudo apt-get remove -y azure-cli google-cloud-sdk hhvm google-chrome-stable firefox powershell mono-devel
-sudo apt-get autoremove -y
-sudo apt-get clean
-df -h
+#sudo apt-get autoremove -y
+#sudo apt-get clean
+#echo ""
+#echo "Disk space after apt-get:"
+#df -h
 
+# Large dirs
 echo "Removing large directories"
 # https://github.com/apache/flink/blob/master/tools/azure-pipelines/free_disk_space.sh
-rm -rf /usr/share/dotnet/
+sudo rm -rf /usr/share/dotnet
+sudo rm -rf /usr/local/lib/android
+sudo rm -rf /opt/ghc
+echo ""
+echo "Disk space after removing large directories:"
+df -h
 
 # https://github.com/actions/virtual-environments/issues/709#issuecomment-612569242
-rm -rf "/usr/local/share/boost"
-rm -rf "$AGENT_TOOLSDIRECTORY"
+echo "Removing preinstalled tools"
+sudo rm -rf "/usr/local/share/boost"
+sudo rm -rf "$AGENT_TOOLSDIRECTORY"
+echo ""
 
 # After
 echo "Disk space after:"
