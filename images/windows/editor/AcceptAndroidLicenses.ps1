59095b8ee11f8ac81b6b88c109a4d2155772ae92
Andrew Kahr <22359829+AndrewKahr@users.noreply.github.com>
2023-10-16 00:12:19 -0700

    Accept Android licenses

---
+++ images/windows/editor/AcceptAndroidLicenses.ps1
@@ -0,0 +1,3 @@
+# Accept Android Licenses
+Set-Location $Env:ANDROID_CMDLINE_TOOLS/bin
+bash -c "yes | ./sdkmanager.bat --licenses"
---
+++ images/windows/editor/SetupAndroid.ps1
@@ -0,0 +1,42 @@
+function Get-Module-Destination-Path {
+
+    param (
+        $ModuleList,
+        $ModuleID
+    )
+    $index=$ModuleList.FindIndex( {$args[0].id.contains($ModuleID)} )
+    $rawPath=$ModuleList[$index].destination
+    return $rawPath.Replace('{UNITY_PATH}', $Env:UNITY_PATH)
+}
+
+function Get-Module-Renamed-Path {
+
+  param (
+      $ModuleList,
+      $ModuleID
+  )
+  $index=$ModuleList.FindIndex( {$args[0].id.contains($ModuleID)} )
+  $rawPath=$ModuleList[$index].extractedPathRename.to
+  return $rawPath.Replace('{UNITY_PATH}', $Env:UNITY_PATH)
+}
+
+# Read the modules.json file for the editor to figure out the proper paths dynamically
+[void][System.Reflection.Assembly]::LoadWithPartialName("System.Web.Extensions")
+$raw_modules = Get-Content "$Env:UNITY_PATH/modules.json"
+$UNITY_MODULES_JSON = (New-Object -TypeName System.Web.Script.Serialization.JavaScriptSerializer -Property @{MaxJsonLength=67108864}).DeserializeObject($raw_modules)
+$UNITY_MODULES_LIST = [Collections.Generic.List[Object]]($UNITY_MODULES_JSON)
+
+# Find our environment variables
+$ANDROID_SDK_ROOT = Get-Module-Destination-Path $UNITY_MODULES_LIST 'android-sdk-platform-tools'
+$ANDROID_NDK_HOME = Get-Module-Destination-Path $UNITY_MODULES_LIST 'android-ndk'
+$JAVA_HOME = Get-Module-Destination-Path $UNITY_MODULES_LIST 'android-open-jdk'
+$CMDLINE_TOOLS_PATH = Get-Module-Renamed-Path $UNITY_MODULES_LIST 'android-sdk-command-line-tools'
+
+# Set our environment variables
+$newPath = "$JAVA_HOME/bin;$ANDROID_SDK_ROOT/tools;$CMDLINE_TOOLS_PATH/bin;$ANDROID_SDK_ROOT/platform-tools;$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/windows-x86_64/bin;C:/Program Files/Git/bin"
+$oldPath = [Environment]::GetEnvironmentVariable('PATH', 'Machine');
+[Environment]::SetEnvironmentVariable('PATH', "$newPath;$oldPath",'Machine');
+[Environment]::SetEnvironmentVariable('ANDROID_HOME', $ANDROID_SDK_ROOT,'Machine');
+[Environment]::SetEnvironmentVariable('ANDROID_NDK_HOME', $ANDROID_NDK_HOME,'Machine');
+[Environment]::SetEnvironmentVariable('JAVA_HOME', $JAVA_HOME,'Machine');
+[Environment]::SetEnvironmentVariable('ANDROID_CMDLINE_TOOLS', $CMDLINE_TOOLS_PATH,'Machine');
