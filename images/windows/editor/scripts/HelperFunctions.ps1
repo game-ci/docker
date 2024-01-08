33cc94c160e496034f2b72b39a424b5a5daffbe2
Andrew Kahr <22359829+AndrewKahr@users.noreply.github.com>
2024-01-08 06:38:45 -0800

    Fix Image Build Errors on 2023.2.4f1+ (#236)
    * Bump artifact upload/download versions. Fix for new Unity modules.json format
    
    * Update test.yml to conform with new activation requirements
    
    * Revert artifact action to v3 due to intermittent issues
    
    * Move minimum test version to 2019.3
    
    * Only do test builds when not a PR due to needing access to secrets
    
    * Bump ubuntu hub version, cancel superseded checks

--- images/windows/editor/scripts/HelperFunctions.ps1
+++ images/windows/editor/scripts/HelperFunctions.ps1
@@ -6,15 +6,15 @@ function CheckLastExitCode {
   param ([int[]]$SuccessCodes = @(0), [scriptblock]$CleanupScript=$null)
 
   if ($SuccessCodes -notcontains $LastExitCode) {
-      if ($CleanupScript) {
-          "Executing cleanup script: $CleanupScript"
-          &$CleanupScript
-      }
-      $msg = @"
+    if ($CleanupScript) {
+      "Executing cleanup script: $CleanupScript"
+      &$CleanupScript
+    }
+    $msg = @"
 EXE RETURNED EXIT CODE $LastExitCode
 CALLSTACK:$(Get-PSCallStack | Out-String)
 "@
-      throw $msg
+    throw $msg
   }
 }
 
@@ -25,24 +25,37 @@ CALLSTACK:$(Get-PSCallStack | Out-String)
 function Get-ModuleDestinationPath {
 
   param (
-      $ModuleList,
-      $ModuleID,
-      $UnityPath
+    $ModuleList,
+    $ModuleID,
+    $UnityPath
   )
   $index=$ModuleList.FindIndex( {$args[0].id.contains($ModuleID)} )
   $rawPath=$ModuleList[$index].destination
+
+  if ($null -eq $rawPath) {
+    throw "Unable to get a destination path for $ModuleID"
+  }
+
   return $rawPath.Replace('{UNITY_PATH}', $UnityPath)
 }
 
 function Get-ModuleRenamedPath {
 
 param (
-    $ModuleList,
-    $ModuleID,
-    $UnityPath
+  $ModuleList,
+  $ModuleID,
+  $UnityPath
 )
 $index=$ModuleList.FindIndex( {$args[0].id.contains($ModuleID)} )
 $rawPath=$ModuleList[$index].extractedPathRename.to
+
+if ($null -eq $rawPath) {
+  $rawPath=$ModuleList[$index].renameTo
+  if ($null -eq $rawPath) {
+    throw "Unable to get a ModuleRenamedPath for $ModuleID"
+  }
+}
+
 return $rawPath.Replace('{UNITY_PATH}', $UnityPath)
 }
 
